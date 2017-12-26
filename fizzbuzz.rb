58c719dbf0ef7d1b6ef06e9657e5214430c80551
Ingrid Wong <yw340@georgetown.edu>
2017-12-26 03:36:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,16 @@
-def fizzbuzz
+def fizzbuzz(n)
+  if n % 3 == 0
+    puts ''
+  end
+  elsif n % 5 == 0
+  puts ''
+  end
+elsif n % 3 == 0 && n % 5 == 0 
+  puts ''
+end
+else 
+  puts ''
+end
+  
 
 end
